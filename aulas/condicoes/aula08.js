d48d879c9bf33d3b2254e03e55d6f579f682ac1d
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-05-02 10:13:07 -0300

    Criei um aporrada de cosas

---
+++ aulas/condicoes/aula08.js
@@ -0,0 +1,5 @@
+var vel=62;
+console.log(`Sua velocidade é de ${vel}km/h.`)
+if(vel > 60){
+    console.log('MULDADO, você está a cima de 60km/h')
+}
---
+++ aulas/condicoes/ex009.js
@@ -0,0 +1,8 @@
+var pais='brasil';
+
+if(pais=='brasil'){
+    console.log('Tu es brasileiro.')
+}
+else{
+    console.log('Tu es gringo')
+}
