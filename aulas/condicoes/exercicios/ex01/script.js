d87ce376e3711ffbb62595fbd1f28cf92496a2ab
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-06-08 08:45:56 -0300

    Mudou muita coisa aí

--- aulas/condicoes/exercicios/ex01/script.js
+++ aulas/condicoes/exercicios/ex01/script.js
@@ -1,1 +1,20 @@
-alert('Ola')
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var imk = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours(); 
+    /* var hora = 22 */
+    msg.innerHTML = `Agora são ${hora} horas.`;
+     
+    if(hora >= 0 && hora < 12){
+        /* Bom dia */
+        imk.src = 'imagens/manha.jpg'
+    }else if(hora >= 12 && hora < 18){
+        /* Boa tarde */
+        imk.src = 'imagens/tarde.jpg'
+    }else{
+        /* Boa noite */
+        imk.src ='imagens/noite.jpg'
+    }
+};
+
