1769642e015bd974be65912b088ad4ef6fce3bec
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-06-14 11:08:46 -0300

    Aula de repetição

---
+++ aulas/repetição/aula13.js
@@ -0,0 +1,23 @@
+var c = 1; /* Criando contador */
+
+while (c <= 11)/*Enquanto c for menor ou igual a 6 */{
+    console.log(`Passo ${c}`)
+    c++ /* (c = c + 1) C recebe c mais 1*/
+}
+
+var d = 1;
+while (true){
+    d++
+    console.log(`Infinito mesmo ${d}`)
+    if (d==10){
+        break
+    }
+}
+
+ c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++ 
+} while(c <= 6)
+
+
---
+++ aulas/repetição/aula14.js
@@ -0,0 +1,8 @@
+function pri(p){
+    console.log(p)
+}
+console.log('Vai começar')
+for(var c = 1;c <= 5;c++){
+    console.log(`${c}`)
+}
+pri('Fim')
