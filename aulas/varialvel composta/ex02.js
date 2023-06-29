217c908a0ce85c21b6988fb8ff577b46a3fc752a
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-06-29 12:01:53 -0300

    Criação de ARRYS

---
+++ aulas/varialvel composta/ex02.js
@@ -0,0 +1,13 @@
+let num=[3,4,5,6,134,12]
+num.sort()
+console.log(num)
+num.push(1)
+num.sort()
+
+/* for (let pos =0;pos<num.length;pos++){
+    console.log(num[pos])
+}
+for(let pos in num ){
+    console.log(num[pos])
+} */
+console.log(num.indexOf(3))/* Exite o valor 3 no vetor 3 */
